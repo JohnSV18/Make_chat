bc681092076c1d17cfc1f702df4ab46590010149
John Marcos <johnsaguay@gmail.com>
2021-10-02 22:27:31 -0700

    Messages go to correct channel

--- public/index.js
+++ public/index.js
@@ -21,6 +21,8 @@ $(document).ready(()=>{
   
     $('#send-chat-btn').click((e) => {
       e.preventDefault();
+       // Get the client's channel
+      let channel = $('.channel-current').text();
       // Get the message text value
       let message = $('#chat-input').val();
       // Make sure it's not empty
@@ -29,6 +31,8 @@ $(document).ready(()=>{
         socket.emit('new message', {
           sender : currentUser,
           message : message,
+          //Send the channel over to the server
+          channel : channel
         });
         $('#chat-input').val("");
       }
@@ -52,13 +56,17 @@ $(document).ready(()=>{
 
     //Output the new message
     socket.on('new message', (data) => {
-      $('.message-container').append(`
-        <div class="message">
+      //Only append the message if the user is currently in that channel
+      let currentChannel = $('.channel-current').text();
+      if(currentChannel == data.channel) {
+        $('.message-container').append(`
+          <div class="message">
             <p class="message-user">${data.sender}: </p>
             <p class="message-text">${data.message}</p>
-        </div>
+          </div>
         `);
-  })
+      }
+    });
     socket.on('get online users', (onlineUsers) => {
       //You may have not have seen this for loop before. It's syntax is for(key in obj)
       //Our usernames are keys in the object of onlineUsers.
@@ -72,6 +80,29 @@ $(document).ready(()=>{
       for(username in onlineUsers){
         $('.users-online').append(`<p>${username}</p>`);
       }
+    });
+
+    // Add the new channel to the channels list (Fires for all clients)
+    socket.on('new channel', (newChannel) => {
+      $('.channels').append(`<div class="channel">${newChannel}</div>`);
+    });
+  
+    // Make the channel joined the current channel. Then load the messages.
+    // This only fires for the client who made the channel.
+    socket.on('user changed channel', (data) => {
+      $('.channel-current').addClass('channel');
+      $('.channel-current').removeClass('channel-current');
+      $(`.channel:contains('${data.channel}')`).addClass('channel-current');
+      $('.channel-current').removeClass('channel');
+      $('.message').remove();
+      data.messages.forEach((message) => {
+        $('.message-container').append(`
+          <div class="message">
+            <p class="message-user">${message.sender}: </p>
+            <p class="message-text">${message.message}</p>
+          </div>
+        `);
+      });
     });
   
   })
